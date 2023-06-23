f5055df02215aa4ad846e1b972e8bb27f84552e1
gm-mehdi <126019303+gm-mehdi@users.noreply.github.com>
2023-06-23 17:18:06 +0100

    Update Home.js
    -Added a loading feature that enhances the user experience by displaying a loading icon while the weather information is being fetched.
    
    -This loading icon provides visual feedback and assures users that the app is actively working to retrieve the requested information.
    
    -The error message no longer remains visible after entering a valid city name, as it has been fixed.

--- src/Home.js
+++ src/Home.js
@@ -4,11 +4,67 @@ const Home = () => {
   const [data, setData] = useState({});
   const [city, setCity] = useState("");
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [isPending, setIsPending] = useState(false);
 
   const HandleClick = () => {
     const trimmedCity = city.trim();
     if (trimmedCity !== "") {
       const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${trimmedCity}&appid={YOUR_API_KEY}&units=metric`;
+      setIsPending(true);
+      axios
+        .get(apiURL)
+        .then((response) => {
+          let iconweather = "";
+          switch (response.data.weather[0].main) {
+            case "Clear":
+              iconweather = "/Images/clear.png";
+              break;
+            case "Clouds":
+              iconweather = "/Images/clouds.png";
+              break;
+            case "Haze":
+              iconweather = "/Images/mist.png";
+              break;
+            case "Drizzle":
+            case "Rain":
+              iconweather = "/Images/drizzle&rain.png";
+              break;
+            case "Thunderstorm":
+              iconweather = "/Images/thunderstorm.png";
+              break;
+            case "Snow":
+              iconweather = "/Images/snow.png";
+              break;
+            case "Mist":
+              iconweather = "/Images/Mist.png";
+              break;
+            default:
+              break;
+          }
+          //console.log(response.data);
+          setTimeout(() => {
+            setData({
+              celcius: response.data.main.temp,
+              name: response.data.name,
+              humidity: response.data.main.humidity,
+              speed: response.data.wind.speed,
+              image: iconweather,
+            });
+            setIsPending(false);
+          }, 500);
+        })
+        .catch((err) => {
+          console.log(err);
+          if (err.response.status === 404) {
+            setError("Please type a valid city name");
+          } else setError("");
+        });
+    }
+  };
+  if (city === "") {
+    const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=agadir&appid={YOUR_API_KEY}&units=metric`;
+    setTimeout(() => {
       axios
         .get(apiURL)
         .then((response) => {
@@ -47,57 +103,10 @@ const Home = () => {
             speed: response.data.wind.speed,
             image: iconweather,
           });
+          setIsLoading(false);
         })
-        .catch((err) => {
-          console.log(err);
-          if (err.response.status === 404) {
-            setError("Please type a valid city name");
-          } else setError("");
-        });
-    }
-  };
-  if (city === "") {
-    const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=agadir&appid={YOUR_API_KEY}&units=metric`;
-    axios
-      .get(apiURL)
-      .then((response) => {
-        let iconweather = "";
-        switch (response.data.weather[0].main) {
-          case "Clear":
-            iconweather = "/Images/clear.png";
-            break;
-          case "Clouds":
-            iconweather = "/Images/clouds.png";
-            break;
-          case "Haze":
-            iconweather = "/Images/mist.png";
-            break;
-          case "Drizzle":
-          case "Rain":
-            iconweather = "/Images/drizzle&rain.png";
-            break;
-          case "Thunderstorm":
-            iconweather = "/Images/thunderstorm.png";
-            break;
-          case "Snow":
-            iconweather = "/Images/snow.png";
-            break;
-          case "Mist":
-            iconweather = "/Images/Mist.png";
-            break;
-          default:
-            break;
-        }
-        //console.log(response.data);
-        setData({
-          celcius: response.data.main.temp,
-          name: response.data.name,
-          humidity: response.data.main.humidity,
-          speed: response.data.wind.speed,
-          image: iconweather,
-        });
-      })
-      .catch((err) => console.log(err));
+        .catch((err) => console.log(err));
+    }, 500);
   }
 
   return (
@@ -109,34 +118,45 @@ const Home = () => {
             placeholder="Enter a city name"
             onChange={(e) => setCity(e.target.value)}
           />
-          <button>
-            <img src="/Images/search.png" alt="" onClick={HandleClick} />
+          <button onClick={HandleClick}>
+            <img src="/Images/search.png" alt="" />
           </button>
         </div>
-        <div className="error">
-          <p>{error}</p>
-        </div>
-        <div className="w_info">
-          <img src={data.image} alt="clear" className="icon" />
-          <h1>{Math.round(data.celcius)} °C</h1>
-          <h2>{data.name}</h2>
-          <div className="details">
-            <div className="col">
-              <img src="/Images/humidity.png" alt="humidity" />
-              <div className="humidity">
-                <p>{data.humidity}%</p>
-                <p>Humidity</p>
+        {isLoading || isPending ? (
+          <div className="error">
+            <p>{error}</p>
+          </div>
+        ) : (
+          <div></div>
+        )}
+
+        {isLoading || isPending ? (
+          <div className="loading">
+            <div className="spinner "></div>
+          </div>
+        ) : (
+          <div className="w_info">
+            <img src={data.image} alt="clear" className="icon" />
+            <h1>{Math.round(data.celcius)} °C</h1>
+            <h2>{data.name}</h2>
+            <div className="details">
+              <div className="col">
+                <img src="/Images/humidity.png" alt="humidity" />
+                <div className="humidity">
+                  <p>{data.humidity}%</p>
+                  <p>Humidity</p>
+                </div>
               </div>
-            </div>
-            <div className="col">
-              <img src="/Images/wind.png" alt="wind" />
-              <div className="wind">
-                <p>{Math.round(data.speed)} km/h</p>
-                <p>Wind</p>
+              <div className="col">
+                <img src="/Images/wind.png" alt="wind" />
+                <div className="wind">
+                  <p>{Math.round(data.speed)} km/h</p>
+                  <p>Wind</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
